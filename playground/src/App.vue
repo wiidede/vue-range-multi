a3914c1947971f4f7770540aba923c5187051713
wiidede <wiixdede@gmail.com>
2023-10-09 23:30:55 +0800

    feat: add playground

---
+++ playground/src/App.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { Range } from 'vue-range-multi'
+import { ref } from 'vue'
+
+const model = ref<any[]>([])
+</script>
+
+<template>
+  <Range
+    v-model="model"
+    class="w-full pt16 pb8"
+    :max-thumb="6"
+  />
+</template>
+
+<style scoped>
+
+</style>
