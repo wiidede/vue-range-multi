a5772c7c6cdfa4557f7168db2788829b42aa5070
wiidede <wiixdede@gmail.com>
2023-10-11 16:12:27 +0800

    chore: sourcemap

--- vite.config.ts
+++ vite.config.ts
@@ -19,6 +19,7 @@ export default defineConfig({
     }),
   ],
   build: {
+    sourcemap: true,
     lib: {
       // Could also be a dictionary or array of multiple entry points
       entry: resolve(__dirname, 'src/index.ts'),
