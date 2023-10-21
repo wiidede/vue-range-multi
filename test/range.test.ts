92cfba3424a923fca0712da92237ce1aac098fcb
wiidede <wiixdede@gmail.com>
2023-10-21 18:41:33 +0800

    chore: fix lint

--- test/range.test.ts
+++ test/range.test.ts
@@ -1,6 +1,6 @@
 import { describe, expect, it } from 'vitest'
 import { mount } from '@vue/test-utils'
-import { h, nextTick } from 'vue'
+import { h } from 'vue'
 import { Range } from '../src/index'
 
 describe('range', () => {
