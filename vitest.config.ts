13da7e068a6f82a69fbddedde70b81eb58f9afa3
wiidede <wiixdede@gmail.com>
2024-12-12 18:26:12 +0800

    chore: fix test config

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,11 +1,9 @@
-import { defineConfig } from 'vitest/config'
+import { defineConfig, mergeConfig } from 'vitest/config'
+import viteConfig from './vite.config'
 
-export default defineConfig({
+export default mergeConfig(viteConfig, defineConfig({
   test: {
     include: ['test/**/*.test.ts'],
     environment: 'jsdom',
-    // deps: {
-    //   inline: ['@vue'],
-    // },
   },
-})
+}))
